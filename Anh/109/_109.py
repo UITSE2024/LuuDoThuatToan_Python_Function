25f18f4ebd8e330060f1eef7785f5adacab9e8b8
AnhGam <guardalex8@gmail.com>
2025-07-04 14:59:58 +0700

    109

---
+++ Anh/109/_109.py
@@ -0,0 +1,19 @@
+def Euler():
+    s=1
+    m=1
+    e=1
+    i=1
+    while(e>10**-6):
+        m=m*i
+        e=1/m
+        s+=e
+        i+=1
+    return s
+
+def main():
+    s=Euler()
+    print("Euler =", s)
+
+if __name__ == "__main__":
+    main()
+
