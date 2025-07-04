5905347c4273d164a6f600403bbea6908efd3832
LeonLH928 <lehuynhanhtuan123@gmail.com>
2025-07-04 14:41:55 +0700

    Add 016

---
+++ Tuan/016/_016.py
@@ -0,0 +1,20 @@
+from re import X
+
+
+def Nhap():
+    xx = int(input("Nhap x = "))
+    return xx
+
+def Tinh(xx):
+    x2 = xx * xx
+    x4 = x2 * x2
+    x8 = x4 * x4
+    x9 = x8 * X
+    return x9
+
+def main():
+    x = Nhap()
+    Tinh(x)
+
+if __name__ == "__main__":
+    main()
