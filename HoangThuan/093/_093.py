be9408bc20547880afb416e55f38925bd5cad846
NightMareScorn <NightMareScorn@gmail.com>
2025-07-04 15:41:15 +0700

    093

---
+++ HoangThuan/093/_093.py
@@ -0,0 +1,20 @@
+
+from math import sqrt
+
+def TinhSn(nn):
+    s = 0
+    i = 1
+
+    while i <= nn:
+        s = sqrt(2 + s)
+        i = i + 1
+    
+    return s
+
+def main():
+    n = int(input("Nhap n: "))
+
+    print("S(n) =", TinhSn(n))
+
+if __name__ == "__main__":
+    main()
