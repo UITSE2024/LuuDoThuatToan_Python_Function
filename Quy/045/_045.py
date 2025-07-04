d26506417d31e27dabe05d599e75d4cd3a2bcac1
QusyPlus <duongnguyenphuquy17052000@gmail.com>
2025-07-04 14:54:46 +0700

    045

---
+++ Quy/045/_045.py
@@ -0,0 +1,14 @@
+def Tong(n):
+    s = 0
+    i = 1
+    while (i <= n):
+        s = s + 1 / (i ** (1 / 2) + (i + 1) ** (1 / 2))
+        i = i + 1
+    return s
+
+def main():
+    n = int(input("Nhap n: "))
+    print("Tong la: ", Tong(n))
+
+if __name__ == "__main__":
+    main()
