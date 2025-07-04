3bf4f11a3181370a12d272ff0c904a86c2962bfe
Hoang Duc Chinh <hoangducchinh730@gmail.com>
2025-07-04 14:48:06 +0700

    Chinh lam 040

---
+++ Chinh/040/_040.py
@@ -0,0 +1,15 @@
+def Tinh(nn):
+    s = 0
+    i = 1
+    while(i <= nn):
+        s += i * (i + 1)
+        i += 1
+    return s
+
+def main():
+    n = int(input("Nhap n: "))
+
+    print("Ket qua: ", Tinh(n));
+
+if __name__ == "__main__":
+    main()
