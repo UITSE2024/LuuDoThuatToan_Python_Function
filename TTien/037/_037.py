96f1c787a1c28451e3ef7b090406e17182d7ec5e
Thao-Tien <lilianjung2036@gmail.com>
2025-07-04 14:56:56 +0700

    037

--- TTien/037/_037.py
+++ TTien/037/_037.py
@@ -1,18 +1,14 @@
 
-def Nhap():
-    n = int(input("Nhap n: "))
-    return n
-
 def Tinh(nn):
     s = 0
     i = 1
-    while(i<=n):
+    while(i<=nn):
         s = s + i**3
         i+=1
     return s
 
 def main():
-    n = Nhap();
+    n = int(input("Nhap n: "))
     print(Tinh(n))
 
 if __name__=="__main__":
