bc48bdcd2d3b0d42883aa9f980120abc202e7c2a
Nguyễn Đại Hưng <24520601@gm.uit.edu.vn>
2025-07-04 14:58:52 +0700

    fix 034

--- DaiHung/DaiHung/034/_034.py
+++ DaiHung/DaiHung/034/_034.py
@@ -1,18 +1,18 @@
 def Nhap():
-    n = int(input("Nhap n: "))
-    return n
+    nn = int(input("Nhap n: "))
+    return nn
 
-def tinhTong(n):
+def tinhTong(nn):
     s = 0
     i = 1
 
-    while i <= 2 * n + 1:
+    while i <= 2 * nn + 1:
         s = s + (i / (i + 1))
         i = i + 2
     return s
 
-def Xuat(s):
-    print(f"Tong S = {s:.2f}")
+def Xuat(ss):
+    print(f"Tong S = {ss:.2f}")
 
 def main():
     n = Nhap()
